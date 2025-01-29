d1265f2f62021e6c3a3013b41ef90c2283379715
David Altamirano <jdaltamiranodev@gmail.com>
2025-01-29 10:24:20 +0100

    Agregar lógica para guardar el carrito en localStorage y permitir vaciar el carrito desde el componente Header

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-    import { ref, reactive, onMounted } from 'vue';
+    import { ref, reactive, onMounted, watch } from 'vue';
     import { db } from './data/guitarras.js';
     import Guitarra from './components/Guitarra.vue';
     import Header from './components/Header.vue';
@@ -9,10 +9,25 @@
     const carrito = ref([]);
     const guitarra = ref({});
 
+    watch(carrito, () => {
+        guardarLocalStorage();
+    }, {
+        deep: true
+    })
+
     onMounted(() => {
         guitarras.value = db;
         guitarra.value = db[3];
+
+        const carritoStorage = localStorage.getItem('carrito');
+        if(carritoStorage) {
+            carrito.value = JSON.parse(carritoStorage);
+        }
     })
+
+    const guardarLocalStorage = () => {
+        localStorage.setItem('carrito', JSON.stringify(carrito.value));
+    }
 
     const agregarCarrito = (guitarra) => {
         const existeCarrito = carrito.value.findIndex(producto => producto.id === guitarra.id);
@@ -39,6 +54,11 @@
     const eliminarProducto = (id) => {
         carrito.value = carrito.value.filter(producto => producto.id !== id);
     }
+
+    const vaciarCarrito = () => {
+        // console.log('vaciar');
+        carrito.value = [];
+    }
 </script>
 
 <template>
@@ -49,6 +69,7 @@
         @decrementar-cantidad="decrementarCantidad"
         @agregar-carrito="agregarCarrito"
         @eliminar-producto="eliminarProducto"
+        @vaciar-carrito="vaciarCarrito"
     />
   
     <main class="container-xl mt-5">
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -13,7 +13,7 @@
         }  
     });
 
-    defineEmits(['decrementar-cantidad', 'incrementar-cantidad', 'agregar-carrito', 'eliminar-producto']);
+    defineEmits(['decrementar-cantidad', 'incrementar-cantidad', 'agregar-carrito', 'eliminar-producto', 'vaciar-carrito']);
 
     const totalPagar = computed(() => {
         return props.carrito.reduce((total, producto) => total + (producto.cantidad * producto.precio), 0)
@@ -89,7 +89,7 @@
                                 </table>
 
                                 <p class="text-end">Total pagar: <span class="fw-bold">${{ totalPagar }}</span></p>
-                                <button class="btn btn-dark w-100 mt-3 p-2">Vaciar Carrito</button>
+                                <button class="btn btn-dark w-100 mt-3 p-2" @click="$emit('vaciar-carrito')">Vaciar Carrito</button>
                             </div>
                         </div>
                     </div>
